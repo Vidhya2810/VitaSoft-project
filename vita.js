61ff68bf107b334e944d9614c66ee66a8952cd43
Vidhya2810 <87438949+Vidhya2810@users.noreply.github.com>
2021-08-30 11:17:41 +0530

    Update vita.js

--- vita.js
+++ vita.js
@@ -21,17 +21,17 @@ var stateObject = {
     countySel.options[countySel.options.length] = new Option(country, country);
     }
     countySel.onchange = function () {
-    stateSel.length = 1; // remove all options bar first
-    districtSel.length = 1; // remove all options bar first
-    if (this.selectedIndex < 1) return; // done
+    stateSel.length = 1; 
+    districtSel.length = 1; 
+    if (this.selectedIndex < 1) return; 
     for (var state in stateObject[this.value]) {
     stateSel.options[stateSel.options.length] = new Option(state, state);
     }
     }
     countySel.onchange(); // reset in case page is reloaded
     stateSel.onchange = function () {
-    districtSel.length = 1; // remove all options bar first
-    if (this.selectedIndex < 1) return; // done
+    districtSel.length = 1; 
+    if (this.selectedIndex < 1) return; 
     var district = stateObject[countySel.value][this.value];
     for (var i = 0; i < district.length; i++) {
     districtSel.options[districtSel.options.length] = new Option(district[i], district[i]);
